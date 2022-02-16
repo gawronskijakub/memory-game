2c65bee9294902cd1343354a38ef5965f8d80ea1
James <44299334+gawronskijakub@users.noreply.github.com>
2022-02-16 12:29:13 +0100

    Added new fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./fonts/Fredoka_One/FredokaOne-Regular.ttf";
+import "./fonts/Open_Sans_Condensed/OpenSansCondensed-Bold.ttf";
 import "./index.css";
 import App from "./components/App";
 
